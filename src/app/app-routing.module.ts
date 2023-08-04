02f895162d37eb956acfaf94e947f44fc4519b1f
felipebrenno0 <felipebrenno213@gmail.com>
2023-08-03 21:25:27 -0300

    <refactor>delete pages/location component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { LocationComponent } from './pages/location/location.component';
 import { UtilitiesComponent } from './pages/utilities/utilities.component';
 import { AboutComponent } from './pages/about/about.component';
 
@@ -9,10 +8,6 @@ const routes: Routes = [
   {
     path: '',
     component:HomeComponent, pathMatch:'full'
-  },
-  {
-    path: 'location',
-    component:LocationComponent
   },
   {
     path: 'utilities',
